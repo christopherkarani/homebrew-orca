eee69d7f2bca945648b4f5c1bb9b3bedd6314a25
christopherkarani <chrisbkarani@gmail.com>
2026-05-20 11:16:36 +0300

    Update orca to 1.1.1

--- Formula/orca.rb
+++ Formula/orca.rb
@@ -1,31 +1,32 @@
 class Orca < Formula
   desc "Local runtime firewall for AI agents"
   homepage "https://github.com/christopherkarani/Orca"
-  version "1.1.0"
+  version "1.1.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/christopherkarani/Orca/releases/download/v#{version}/orca-v#{version}-darwin-arm64.tar.gz"
-      sha256 "b93a7f5fa5d94700f7f990d06e705bfddfcc67bc4b49c0687a630fdab800bc3d"
+      sha256 "2ac61814f7603bb41a293948fca9a09585c7073458a33c35b3ca3ef7c8892405"
     else
       url "https://github.com/christopherkarani/Orca/releases/download/v#{version}/orca-v#{version}-darwin-amd64.tar.gz"
-      sha256 "fda91b91bff5ef5e5f2c7549b074e7400918c3010d3ec72cf253ce01eff45178"
+      sha256 "28044dd1c3e4b120826ccb3e3787e25048a9a6ddce8ec8b444b84b0085928a7f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/christopherkarani/Orca/releases/download/v#{version}/orca-v#{version}-linux-arm64.tar.gz"
-      sha256 "dabd4f89a49c03d7c3c1a9e96bbfeb828b39903d68b666de9c2ae194c78abecf"
+      sha256 "3c28eef2324f7dbcfafd10ad83ba7707f2c6f4125a3925892898ee25a7f5a116"
     else
       url "https://github.com/christopherkarani/Orca/releases/download/v#{version}/orca-v#{version}-linux-amd64.tar.gz"
-      sha256 "ace518c72134b8deee5816819f31781ebf92bfa64a76a20051181a46cf7cf852"
+      sha256 "3f49e9265bce4f043642b84552d92b8d33f235a0be30a5b82d5d830bf4efa2e0"
     end
   end
 
   def install
     bin.install "bin/orca"
+    prefix.install "orca-dashboard-ui/dist" => "orca-dashboard-ui/dist"
   end
 
   test do
